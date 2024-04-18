3b7828bb964e3b88316f8784244abf3a78e6501a
Muhammad Tahir <muhammadtahirbharchoond@gmail.com>
2024-04-18 16:20:07 +0500

    navbar
    creating navbar for morat site and also added logo image.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className=''>
+        <div className="left__navbar">
+            
+        </div>
+        <div className="right__navbar"></div>
+    </nav>
+  )
+}
+
+export default Navbar
